41e921e6f801417cb1995603da1e32bd6c07350c
Chalermrid Patipanang <chalermrid.patipanang@tesco.com>
2020-12-24 17:05:28 +0700

    deploy to aws lambda

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+StageName=$1
+
+mkdir -p artifact
+npm install
+npm run build
+npm prune --production
+zip -r artifact/luckalot-api.zip dist/ node_modules
+
+aws cloudformation package --template-file template.yaml --s3-bucket luckalot-api.${StageName:-"dev"}-artifact --output-template-file artifact/template.${StageName:-"dev"}.out.yaml
+aws cloudformation deploy --template-file artifact/template.${StageName:-"dev"}.out.yaml --stack-name luckalot-api-${StageName:-"dev"}-stack --capabilities CAPABILITY_NAMED_IAM
