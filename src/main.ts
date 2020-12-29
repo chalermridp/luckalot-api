0fdebe5ad6621669f67e42b537242c39314b3c4b
Chalermrid Patipanang <chalermrid.patipanang@tesco.com>
2020-12-29 16:04:36 +0700

    introduce to configuration files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const serverPort = config.get('server.port');
+  await app.listen(serverPort);
 }
 bootstrap();
