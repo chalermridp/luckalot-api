d7fc7b5bf682d5e0b88bcc2a672eab582fbd40ed
Chalermrid Patipanang <chalermrid.patipanang@tesco.com>
2020-12-29 14:09:27 +0700

    use injectrepository declarator

--- src/result-types/result-types.service.ts
+++ src/result-types/result-types.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { ResultType } from './result-types.entity';
 import { ResultTypeRepository } from './result-types.repository';
 
 @Injectable()
 export class ResultTypesService {
-  constructor(private resultTypesRepository: ResultTypeRepository) {}
+  constructor(
+    @InjectRepository(ResultTypeRepository)
+    private resultTypesRepository: ResultTypeRepository,
+  ) {}
 
   async getAll(): Promise<ResultType[]> {
     return await this.resultTypesRepository.find();
--- src/result-types/sanook/result-types.sanook-row-number.service.ts
+++ src/result-types/sanook/result-types.sanook-row-number.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { ResultTypeSanookRowNumber } from './result-types.sanook-row-number.entity';
 import { ResultTypeSanookRowNumberRepository } from './result-types.sanook-row-number.repository';
 
 @Injectable()
 export class ResultTypesSanookRowNumberService {
   constructor(
+    @InjectRepository(ResultTypeSanookRowNumberRepository)
     private resultTypeSanookRowNumberRepository: ResultTypeSanookRowNumberRepository,
   ) {}
 
