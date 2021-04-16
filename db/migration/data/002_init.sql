aee69fd3f52f3744eb14a083cbd85e48ac6f5ccb
Chalermrid Patipanang <chalermrid.patipanang@lotusretails.com>
2021-04-16 23:06:22 +0700

    add sql to init import result schedule

---
+++ db/migration/data/002_init.sql
@@ -0,0 +1,32 @@
+insert into result_import_schedule (date, is_completed, created_by)
+values ('2021-05-01', false, 'db.migration');
+insert into result_import_schedule (date, is_completed, created_by)
+values ('2021-05-16', false, 'db.migration');
+insert into result_import_schedule (date, is_completed, created_by)
+values ('2021-06-01', false, 'db.migration');
+insert into result_import_schedule (date, is_completed, created_by)
+values ('2021-06-16', false, 'db.migration');
+insert into result_import_schedule (date, is_completed, created_by)
+values ('2021-07-01', false, 'db.migration');
+insert into result_import_schedule (date, is_completed, created_by)
+values ('2021-07-16', false, 'db.migration');
+insert into result_import_schedule (date, is_completed, created_by)
+values ('2021-08-01', false, 'db.migration');
+insert into result_import_schedule (date, is_completed, created_by)
+values ('2021-08-16', false, 'db.migration');
+insert into result_import_schedule (date, is_completed, created_by)
+values ('2021-09-01', false, 'db.migration');
+insert into result_import_schedule (date, is_completed, created_by)
+values ('2021-09-16', false, 'db.migration');
+insert into result_import_schedule (date, is_completed, created_by)
+values ('2021-10-01', false, 'db.migration');
+insert into result_import_schedule (date, is_completed, created_by)
+values ('2021-10-16', false, 'db.migration');
+insert into result_import_schedule (date, is_completed, created_by)
+values ('2021-11-01', false, 'db.migration');
+insert into result_import_schedule (date, is_completed, created_by)
+values ('2021-11-16', false, 'db.migration');
+insert into result_import_schedule (date, is_completed, created_by)
+values ('2021-12-01', false, 'db.migration');
+insert into result_import_schedule (date, is_completed, created_by)
+values ('2021-12-16', false, 'db.migration');
