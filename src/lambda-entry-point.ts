6f8e3a69759d7b481495242f32e90f0c210596f3
Chalermrid Patipanang <chalermrid.p@gmail.com>
2021-05-18 23:47:59 +0700

    remove no longer use import

--- src/lambda-entry-point.ts
+++ src/lambda-entry-point.ts
@@ -1,12 +1,11 @@
 import { Server } from 'http';
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Context } from 'aws-lambda';
 import * as serverlessExpress from 'aws-serverless-express';
 import * as express from 'express';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
-import { ClassSerializerInterceptor } from '@nestjs/common';
 
 let lambdaProxy: Server;
 
@@ -18,7 +17,6 @@ async function bootstrap() {
   );
   nestApp.useGlobalInterceptors(
     new LoggingInterceptor(),
-    new ClassSerializerInterceptor(nestApp.get(Reflector))
   );
   await nestApp.init();
   return serverlessExpress.createServer(expressServer);
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
-import { ClassSerializerInterceptor } from '@nestjs/common';
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
@@ -8,7 +7,6 @@ async function bootstrap() {
   const serverPort = process.env.SERVER_PORT || 3001;
   app.useGlobalInterceptors(
     new LoggingInterceptor(),
-    new ClassSerializerInterceptor(app.get(Reflector))
   );
   await app.listen(serverPort);
 }
