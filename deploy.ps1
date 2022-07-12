eae530193c6e9872bd17f137e8137cf25bf7e462
Chalermrid Patipanang <chalermrid.p@gmail.com>
2022-07-13 02:10:59 +0700

    add --no-fail-on-empty-changeset

--- deploy.ps1
+++ deploy.ps1
@@ -19,6 +19,6 @@ $compress = @{
 Compress-Archive @compress
 
 aws cloudformation package --template-file template.yaml --s3-bucket luckalot-api-artifact --output-template-file artifact/template.$StageName.out.yaml --profile luckalot.$StageName
-aws cloudformation deploy --template-file artifact/template.$StageName.out.yaml --stack-name luckalot-api-stack --capabilities CAPABILITY_NAMED_IAM --profile luckalot.$StageName
+aws cloudformation deploy --template-file artifact/template.$StageName.out.yaml --stack-name luckalot-api-stack --capabilities CAPABILITY_NAMED_IAM --profile luckalot.$StageName --no-fail-on-empty-changeset
 
 Remove-Item "artifact" -Recurse
